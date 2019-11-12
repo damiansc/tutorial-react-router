a7aedf8435b5a41e50d037d6e27b8315eeeca660
damiansc <damiansc0211@gmail.com>
2019-11-12 15:15:07 +0100

    Updates

---
+++ precache-manifest.e1df8e75833022980800015f6691de7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c5de51bef93ac9600d3",
+    "url": "/tutorial-react-router/static/css/main.cca1bd6a.chunk.css"
+  },
+  {
+    "revision": "9c5de51bef93ac9600d3",
+    "url": "/tutorial-react-router/static/js/main.e4666a14.chunk.js"
+  },
+  {
+    "revision": "42f6b6129b3ca0c8aae0",
+    "url": "/tutorial-react-router/static/js/runtime~main.a7e0b5e4.js"
+  },
+  {
+    "revision": "e8e239ccb523469ddb35",
+    "url": "/tutorial-react-router/static/js/2.7343ff84.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/tutorial-react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/tutorial-react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/tutorial-react-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "d4c95506b09bb99b802a3cca9fb0d5ef",
+    "url": "/tutorial-react-router/index.html"
+  }
+];
